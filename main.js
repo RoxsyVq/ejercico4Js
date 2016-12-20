a4c2333feaf8827072b6e44099cccf4ac154282d
paola <lizbethochoa163@gmail.com>
2016-12-20 11:01:26 -0500

    finalizado

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var tiempo, costominuto,costollamada ,
 tiempo=prompt("ingresa porfavor el tiempo de llamada");
 costominuto=prompt("ingrese porfavor el costo por minuto");
-    costollamada= tiempo * costominuto;
+    costollamada= tiempo * costominuto,
     
     alert(costollamada);
+document.write("su costo de llamada es " +  costollamada);
